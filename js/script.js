a857f581180709126d7c92bf5b1544bb57b08c53
Michael Zhao <mweiyuzhao@gmail.com>
2017-09-10 16:20:01 -0400

    changed formating

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $("#PCICalc-go").click(function() {
       var newPCI = $("#PCICalc-selecter").val();
       if(newPCI === "Option 1: Calculator w/ 8 Basic Attributes"){
-        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalc8Basic/");
+        $("#PCICalc-go").attr("href", "8Basic/index.html");
       }
       else if(newPCI === "Option 2: Calculator w/ 4 Attributes Including Annual Precipitation") {
-        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalc4P/");
+        $("#PCICalc-go").attr("href", "4P/index.html");
       }
       else if(newPCI === "Option 3: Calculator w/ 4 Attributes Including Type of Maintenance/Rehabilitation") {
-        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalc4MR/");
+        $("#PCICalc-go").attr("href", "4MR/index.html");
       }
       else {
         $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalcHub");
