1984e17747ca487777c6f76eb51c813785d99f16
Michael Zhao <mweiyuzhao@gmail.com>
2017-09-11 11:39:32 -0400

    added 2 new calculators

--- 4P/js/script.js
+++ 4P/js/script.js
@@ -23,6 +23,12 @@ $(document).ready(function(){
     else if(newPCI === "Option 3: Calculator w/ 4 Attributes Including Type of Maintenance/Rehabilitation") {
       $("#PCICalc-go").attr("href", "../4MR/index.html");
     }
+    else if(newPCI === "Option 4: Calculator w/ 9 Attributes with GBE and 5 Classes") {
+      $("#PCICalc-go").attr("href", "../9AGBE5C/index.html");
+    }
+    else if(newPCI === "Option 5: Calculator w/ 10 Attributes with GBE and Traffic and 5 Classes") {
+      $("#PCICalc-go").attr("href", "../10AGBET5C/index.html");
+    }
     else {
       $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalcHub");
     }
